0a8377df8e1aa38b97d3dcf18a29d51db59546de
Anton Akentiev <anthony.akentiev@gmail.com>
2016-05-25 17:23:23 +0300

    Adding message and blockchain

---
+++ Message.js
@@ -0,0 +1,102 @@
+var private = {}, self = null,
+    library = null, modules = null;
+
+function Message(cb, _library) {
+     self = this;
+     self.type = 7;
+     library = _library;
+     cb(null, self);
+}
+
+Message.prototype.create = function (data, trs) {
+     // recipient
+     trs.recipientId = data.recipientId;
+
+     // Create transaction container
+     trs.asset = {
+          message: new Buffer(data.message, 'utf8').toString('hex') // Save message as hex string
+     };
+     return trs;
+}
+
+Message.prototype.calculateFee = function (trs) {
+     return 100000000;
+}
+
+Message.prototype.verify = function (trs, sender, cb, scope) {
+     // Check if message length is greater than 320 characters
+     if (trs.asset.message.length > 320) {
+          return setImmediate(cb, "Max length of message is 320 characters");
+     }
+
+     setImmediate(cb, null, trs);
+}
+
+Message.prototype.getBytes = function (trs) {
+     return new Buffer(trs.asset.message, 'hex');
+}
+
+Message.prototype.apply = function (trs, sender, cb, scope) {
+     setImmediate(cb);
+}
+
+Message.prototype.undo = function (trs, sender, cb, scope) {
+     setImmediate(cb);
+}
+
+Message.prototype.applyUnconfirmed = function (trs, sender, cb, scope) {
+     setImmediate(cb);
+}
+
+Message.prototype.undoUnconfirmed = function (trs, sender, cb, scope) {
+     setImmediate(cb);
+}
+
+Message.prototype.ready = function (trs, sender, cb, scope) {
+     setImmediate(cb);
+}
+
+Message.prototype.save = function (trs, cb) {
+     modules.api.sql.insert({
+          table: "asset_messages",
+          values: {
+               transactionId: trs.id,
+               message: trs.asset.message
+          }
+     }, cb);
+}
+
+Message.prototype.dbRead = function (row) {
+     if(!row.tm_transactionId) {
+          return null;
+     } else {
+          return {
+               message: row.tm_message
+          };
+     }
+}
+
+Message.prototype.normalize = function (asset, cb) {
+     setImmediate(cb);
+     // Call validator on our asset object
+     library.validator.validate(asset, {
+          type: "object",          // It is an object
+          properties: {
+               message: {          // It contains a message property
+                    type: "string",// It is a string
+                    format: "hex", // It is in a hexadecimal format
+                    minLength: 1   // Minimum length of string is 1 character
+               }
+          },
+          required: ["message"]    // Message property is required and must be defined
+     }, cb);
+}
+
+Message.prototype.onBind = function (_modules) {
+     modules = _modules;
+
+     // inherit
+     modules.logic.transaction.attachAssetType(self.type, self);
+}
+
+module.exports = Message;
